7ef426c70bba77309eb3abb9ca135428810a07d3
unknown <lotaymk@gmail.com>
2025-02-25 23:27:51 -0500

    add components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
+import Landing from "./components/header/Landing";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Landing />} />
         </Routes>
       </Router>
     </>
