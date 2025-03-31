cea628cf708ae8e69f52d28c06382f5fb5ae2ffb
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2025-03-30 21:58:01 -0400

    Singleton

--- backend/db_session.py
+++ backend/db_session.py
@@ -4,19 +4,30 @@ from fastapi import Depends
 
 from sqlmodel import create_engine, SQLModel, Session
 
+from design_patterns.singleton import Singleton
+
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
+
+
+class EngineSingleton(Singleton):
+    def __init__(self, engine):
+        if not hasattr(self, 'engine'):
+            self.engine = engine
+
+
 engine = create_engine(sqlite_url, connect_args=connect_args)
+engine_singleton = EngineSingleton(engine)
 
 
 def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine_singleton.engine)
 
 
 def get_session():
-    with Session(engine) as session:
+    with Session(engine_singleton.engine) as session:
         yield session
 
 
