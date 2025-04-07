8a0c7ad34d8b309c46d636eb753c674cccfd28b5
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2025-04-07 18:41:14 -0400

    Update user.py

--- backend/models/user.py
+++ backend/models/user.py
@@ -39,3 +39,14 @@ class UserCreate(User):
 class UserUpdate(User):
     email: str | None = None
     password: str | None = None
+    points: int | None = None
+    phone_number: str | None = None
+    profile_image: bytes | None = None
+    user_type: UserType | None = None
+    profession: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    affiliation: str | None = None
+    organization_name: str | None = None
+    organization_address: str | None = None
+    is_site_admin: bool | None = None
