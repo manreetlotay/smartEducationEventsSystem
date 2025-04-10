4f9719e6d1f347745b8e1551b028e07b8a9582d9
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2025-04-10 00:20:33 -0400

    Enemy Prefab

---
+++ frontend/src/views/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import ChromeDinoGame from "react-chrome-dino";
+import Footer from "../components/footer/Footer";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <ChromeDinoGame />
+      <Footer />
+    </>
+  );
+}
